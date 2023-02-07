26a7899ff694b489f1e50eb3b2da610fde598d22
Sneha Shukla <72255255+Scarlet-Coder@users.noreply.github.com>
2023-02-08 01:38:57 +0530

    Create uniqueOcc.java

---
+++ Day 6/uniqueOcc.java
@@ -0,0 +1,19 @@
+class uniqueOcc {
+    public boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer,Integer> hs=new HashMap<>();
+        int c=0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length;j++){
+                if(arr[i]==arr[j])
+                    c++;
+            }
+            //System.out.println(c);
+            if(hs.containsKey(c)&& !hs.containsValue(arr[i]))
+                return false;
+            else
+                hs.put(c,arr[i]);
+            c=0;
+        }
+        return true;
+    }
+}
